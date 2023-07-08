b86f9580b09c2dc93d88ecb061e2a75db33b78d9
dhnnz <dhannid001@gmail.com>
2023-07-08 13:26:19 +0700

    Initial commit

---
+++ src/dhnnz/SellGUI/Loader.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace dhnnz\SellGUI;
+
+use cooldogedev\BedrockEconomy\api\BedrockEconomyAPI;
+use cooldogedev\BedrockEconomy\api\legacy\ClosureContext;
+
+use muqsit\invmenu\InvMenu;
+use muqsit\invmenu\InvMenuHandler;
+use muqsit\invmenu\transaction\DeterministicInvMenuTransaction;
+use muqsit\invmenu\transaction\InvMenuTransaction;
+use muqsit\invmenu\transaction\InvMenuTransactionResult;
+
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\event\Listener;
+use pocketmine\inventory\Inventory;
+use pocketmine\item\Item;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+
+class Loader extends PluginBase implements Listener
+{
+
+    public function onEnable(): void
+    {
+        $this->saveResource("config.yml");
+
+        if (!InvMenuHandler::isRegistered()) {
+            InvMenuHandler::register($this);
+        }
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
+    {
+        if (!($sender instanceof Player))
+            return false;
+        if ($command->getName() !== "sell")
+            return false;
+        $this->sellGui($sender);
+        return true;
+    }
+
+    public function getMessage(string $message, array $args = []): string
+    {
+        $replace = $this->getConfig()->get($message, $message);
+
+        for ($i = 0; $i < count($args); $i++) {
+            $replace = str_replace("%$i%", $args[$i], $replace);
+        }
+
+        $replace = TextFormat::colorize($replace);
+        return $replace;
+    }
+
+    public function sellGui(Player $player)
+    {
+        $menu = InvMenu::create(InvMenu::TYPE_DOUBLE_CHEST);
+        $inventory = $menu->getInventory();
+
+        $player->sendMessage($this->getMessage("message.sell.waiting"));
+        $menu->setInventoryCloseListener(function (Player $player, Inventory $inventory) {
+            $config = $this->getConfig();
+            $listSell = [];
+            foreach ($inventory->getContents() as $item) {
+                if (!in_array(str_replace("_", " ", $item->getName()), $config->get("blacklist"))) {
+                    $price = (int) ((isset($config->get("items")[str_replace("_", " ", $item->getName())]["price"])) ? $config->get("items")[str_replace("_", " ", $item->getName())]["price"] : $config->get("items")["default"]["price"]) * $item->getCount();
+                    $listSell[str_replace("_", " ", $item->getName())] = [
+                        "count" => ($listSell[str_replace("_", " ", $item->getName())]["count"] ?? 0) + $item->getCount(),
+                        "price" => ($listSell[str_replace("_", " ", $item->getName())]["price"] ?? 0) + $price
+                    ];
+                } else {
+                    $player->sendMessage($this->getMessage("message.cannot.sell", [str_replace("_", " ", $item->getName())]));
+                    $player->getInventory()->addItem($item);
+                }
+            }
+
+            $prices = array_column($listSell, 'price');
+            array_map(function ($price) use ($player) {
+                BedrockEconomyAPI::legacy()->addToPlayerBalance($player->getName(), $price, ClosureContext::create(fn(bool $updated) => $updated));
+            }, $prices);
+
+            $player->sendMessage(TextFormat::colorize($this->getMessage("message.list.sell.top") . "\n" . implode("\n", array_map(function ($item, $details) {
+                $count = $details['count'];
+                $price = number_format($details['price']);
+                return $this->getMessage("message.list.sell", [$count, $item, $price]);
+            }, array_keys($listSell), $listSell))));
+        });
+
+        $menu->send($player);
+    }
+}
